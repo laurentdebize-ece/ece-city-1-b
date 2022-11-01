e4eb8a3959be0d29f9ef50184aea31502511f56c
Anaé <anae.servant@edu.ece.fr>
2022-11-01 16:53:33 +0200

    test

---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by 33673 on 01/11/2022.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by 33673 on 01/11/2022.
+//
+
+#ifndef ECE_CITY_1_B_MAIN_H
+#define ECE_CITY_1_B_MAIN_H
+
+#endif //ECE_CITY_1_B_MAIN_H
