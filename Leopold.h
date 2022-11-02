cd438af5c1758035dcaaf944d07ae6ec5865eb3f
gabdu <gabriel.duvauchelle@edu.ece.fr>
2022-11-02 16:02:20 +0100

    On commit pas nous ?

---
+++ Leopold.h
@@ -0,0 +1,8 @@
+//
+// Created by gabdu on 02/11/2022.
+//
+
+#ifndef ECE_CITY_1_B_LEOPOLD_H
+#define ECE_CITY_1_B_LEOPOLD_H
+
+#endif //ECE_CITY_1_B_LEOPOLD_H
---
+++ Matthieu.h
@@ -0,0 +1,8 @@
+//
+// Created by gabdu on 02/11/2022.
+//
+
+#ifndef ECE_CITY_1_B_MATTHIEU_H
+#define ECE_CITY_1_B_MATTHIEU_H
+
+#endif //ECE_CITY_1_B_MATTHIEU_H
